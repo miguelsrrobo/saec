873c2c25246e64d44b632340c79893ef6c6f8487
miguel <guegosilvestre67@gmail.com>
2021-10-02 14:56:06 -0300

    adicionando algumas linha no arquivo contato e mudados o style do contato para pasta style

--- src/pages/contato.tsx
+++ src/pages/contato.tsx
@@ -3,5 +3,29 @@ import * as styles from "../styles/contato_style.scss";
 import { Helmet } from "react-helmet";
 
 const ContatoPage = () => {
+  return (
+    <>
+      <Helmet>
+        <title>SAEC 2021 | Em contrução</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Semana Acadêmica de Engenharia de Computação da Universidade Federal de Santa Catarina"
+        />
+      </Helmet>
+      <h3 className={styles.tag}>Em breve!</h3>
+      <main className={styles.main}>
+        <title>SAEC 2021 | Em contrução</title>
 
+        <img className={styles.logo} alt="Logo SAEC" src="/logo.svg" />
+
+        <div className={styles.socialMedia}>
+          <h3>Siga-nos nas redes sociais</h3>
+          <h1>/saecufscara</h1>
+        </div>
+      </main>
+    </>
+  );
 }
+
+export default ContatoPage;
